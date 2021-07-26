4171ce193e449e0033942ebf0df1b86b089e54c3
Shahrizoda Ergasheva <71122864+shagrizoda@users.noreply.github.com>
2021-07-27 00:18:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// For  clicks
 var numberOfButtons=document.querySelectorAll("button").length;
 for(var i=0;i<numberOfButtons;i++)
 {
@@ -9,16 +10,19 @@ for(var i=0;i<numberOfButtons;i++)
   });
 }
 
+// For Keyboard pressing
 document.addEventListener("keypress",function(trigger)
 {
 makeNoise(trigger.key);
 buttonAnimation(trigger.key);
 });
 
+//Connecting sound to buttons
 function makeNoise(key)
 {
   switch(key)
   {
+    //left side, white keys 
     case "a":
       var a4=new Audio("sounds/a4.mp3");
       a4.play();
@@ -29,18 +33,16 @@ function makeNoise(key)
       b4.play();
       break;
 
-      case "d":
+    case "d":
           var a5=new Audio("sounds/a-5.mp3");
           a5.play();
           break;
-
-
-
     case "f":
         var d4=new Audio("sounds/d4.mp3");
         d4.play();
         break;
-
+    
+    //right side,white keys
     case "h":
         var c4=new Audio("sounds/c-4.mp3");
         c4.play();
@@ -59,55 +61,52 @@ function makeNoise(key)
         var f4=new Audio("sounds/f-4.mp3");
         f4.play();
         break;
+     // left side, black keys
+     case "w":
+        var f5=new Audio("sounds/f-5.mp3");
+        f5.play();
+        break;
 
-  // black keys
-  case "w":
-      var f5=new Audio("sounds/f-5.mp3");
-      f5.play();
-      break;
+     case "e":
+        var b5=new Audio("sounds/b5.mp3");
+        b5.play();
+        break;
 
-  case "e":
-      var b5=new Audio("sounds/b5.mp3");
-      b5.play();
-      break;
+     case "r":
+        var c6=new Audio("sounds/c6.mp3");
+        c6.play();
+        break;
 
-  case "r":
-      var c6=new Audio("sounds/c6.mp3");
-      c6.play();
-      break;
+    //right side,black keys 
+    case "u":
+        var g5=new Audio("sounds/g-5.mp3");
+        g5.play();
+        break;
 
-  case "u":
-     var g5=new Audio("sounds/g-5.mp3");
-     g5.play();
-     break;
+     case "i":
+        var d5=new Audio("sounds/d5.mp3");
+        d5.play();
+        break;
 
-  case "i":
-     var d5=new Audio("sounds/d5.mp3");
-     d5.play();
-     break;
+     case "o":
+        var c5=new Audio("sounds/c5.mp3");
+        c5.play();
+        break;
 
-  case "o":
-      var c5=new Audio("sounds/c5.mp3");
-      c5.play();
-      break;
+     case "p":
+        var e5=new Audio("sounds/e5.mp3");
+        e5.play();
+        break;
+   }
+ }
 
-  case "p":
-      var e5=new Audio("sounds/e5.mp3");
-      e5.play();
-      break;
-
-
-  }
-}
-
+//Adding animation to buttons
 function buttonAnimation(currentKey)
 {
   var activeButton=document.querySelector("." + currentKey);
-
   activeButton.classList.add("pressed");
-
   setTimeout(function()
 {
   activeButton.classList.remove("pressed");
-},200);
-}
+  },200);
+ }
